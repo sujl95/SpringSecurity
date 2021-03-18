b048698f3631d668ea1f3e876fb6bcb0c4858dc6
sujl95 <sujl95@naver.com>
2021-03-18 23:00:53 +0900

    init 01 인증 API – 사용자 정의 보안 기능 구현

---
+++ basicsecurity/src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.security.basicsecurity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.anyRequest().authenticated() // 어떤 요청에도 인증
+			.and()
+				.formLogin(); // formLogin 방식으로 로그인
+	}
+}
